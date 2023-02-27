7ba17614c31c66784bd99962412081c207753f2e
pufmat <pufferfish@pufferfish.pl>
2023-02-27 15:42:53 +0100

    Teleport player when outside map bounds

--- src/main/java/net/puffish/snakemod/game/phase/SnakeWaitingPhase.java
+++ src/main/java/net/puffish/snakemod/game/phase/SnakeWaitingPhase.java
@@ -3,6 +3,8 @@ package net.puffish.snakemod.game.phase;
 import com.mojang.datafixers.util.Either;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameMode;
 import net.puffish.snakemod.config.SnakeConfig;
 import net.puffish.snakemod.game.map.SnakeMap;
@@ -61,6 +63,7 @@ public class SnakeWaitingPhase extends SnakePhase {
 
 		activity.listen(GameActivityEvents.REQUEST_START, this::requestStart);
 		activity.listen(GamePlayerEvents.OFFER, this::playerOffer);
+		activity.listen(GameActivityEvents.TICK, this::tick);
 	}
 
 	private GameResult requestStart() {
@@ -73,7 +76,20 @@ public class SnakeWaitingPhase extends SnakePhase {
 
 		return offer.accept(
 				this.world,
-				this.map.getWaitingSpawns().get(random.nextInt(map.getWaitingSpawns().size()))
+				getRandomWaitingSpawn()
 		).and(() -> player.changeGameMode(GameMode.ADVENTURE));
+	}
+
+	private void tick() {
+		gameSpace.getPlayers().forEach(player -> {
+			if(!map.getBounds().contains(new BlockPos(player.getPos()))){
+				Vec3d pos = getRandomWaitingSpawn();
+				player.teleport(pos.x, pos.y, pos.z);
+			}
+		});
+	}
+
+	private Vec3d getRandomWaitingSpawn(){
+		return map.getWaitingSpawns().get(random.nextInt(map.getWaitingSpawns().size()));
 	}
 }
