b33c9e502efbfd6539e5c580c0185ad5d484bf68
ellieisjelly <ellie@ellieis.me>
2026-06-28 11:17:22 -0300

    Update to 26.2

--- src/main/java/net/puffish/snakemod/game/entity/SnakeFoodEntity.java
+++ src/main/java/net/puffish/snakemod/game/entity/SnakeFoodEntity.java
@@ -3,8 +3,9 @@ package net.puffish.snakemod.game.entity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityTypes;
+import net.minecraft.world.entity.monster.cubemob.Slime;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
@@ -13,7 +14,7 @@ public class SnakeFoodEntity extends Slime {
 	public static final double RADIUS = 0.5;
 
 	protected SnakeFoodEntity(Level level) {
-		super(EntityType.SLIME, level);
+		super(EntityTypes.SLIME, level);
 	}
 
 	public static SnakeFoodEntity create(Level level){
